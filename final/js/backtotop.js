ff349e4fd8088066d2939fde01cf1d0685bd3c52
Jake Torgerson <44759116+wackyvert@users.noreply.github.com>
2019-05-29 11:14:47 -0500

    reee

---
+++ final/js/backtotop.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    window.onscroll = function() {scrollFunction()};
+
+
+function scrollFunction() {
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+});
